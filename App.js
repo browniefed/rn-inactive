dc9f94f5c929bd1b1f87508e2f8123cb91c345bb
Jason Brown <browniefed@gmail.com>
2017-12-09 10:48:40 -0800

    Conver to function as child

--- App.js
+++ App.js
@@ -3,22 +3,22 @@ import { Platform, StyleSheet, Text, View, PanResponder } from "react-native";
 import { setTimeout } from "core-js/library/web/timers";
 
 class Inactive extends Component {
+  state = {
+    inactive: false,
+  };
   componentWillMount() {
-    this.inactive = false;
-
     this._panResponder = PanResponder.create({
       onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
         clearTimeout(this.timeout);
 
-        if (this.inactive === true) {
-          this.props.onInactiveChange(false);
+        if (this.state.inactive === true) {
+          this.setState({
+            inactive: false,
+          });
         }
 
-        this.inactive = false;
-
         this.timeout = setTimeout(() => {
-          this.inactive = true;
-          this.props.onInactiveChange(true);
+          this.setState({ inactive: true });
         }, this.props.delay);
       },
     });
@@ -28,30 +28,23 @@ class Inactive extends Component {
   }
 
   render() {
-    const { children, delay, onInactiveChange, ...props } = this.props;
+    const { children, delay, ...props } = this.props;
     return (
       <View {...props} {...this._panResponder.panHandlers}>
-        {children}
+        {children(this.state.inactive)}
       </View>
     );
   }
 }
 
 export default class App extends Component {
-  state = {
-    inactive: false,
-  };
-  handleInactive = isInactive => {
-    this.setState({
-      inactive: isInactive,
-    });
-  };
   render() {
-    const { inactive } = this.state;
     return (
       <View style={styles.container}>
-        <Inactive style={styles.tapMe} delay={1000} onInactiveChange={this.handleInactive}>
-          <Text style={styles.text}>{inactive ? "We're inactive" : "Waiting..."}</Text>
+        <Inactive style={styles.tapMe} delay={1000}>
+          {inactive => {
+            return <Text style={styles.text}>{inactive ? "We're inactive" : "Waiting..."}</Text>;
+          }}
         </Inactive>
       </View>
     );
